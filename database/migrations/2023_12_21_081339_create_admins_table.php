40593c52f897e7299e3dbcaa98a1984b71479264
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-21 09:19:18 +0100

    update migration

--- database/migrations/2023_12_21_081339_create_admins_table.php
+++ database/migrations/2023_12_21_081339_create_admins_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
