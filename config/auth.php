f94cc9d86e87b9c8959be9a9724fbba345dc9faf
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-21 09:37:58 +0100

    add guard

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,16 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+
+        'eleve' => [
+            'driver' => 'session',
+            'provider' => 'eleves',
+        ],
     ],
 
     /*
@@ -63,6 +73,16 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
+
+        'eleves' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Eleve::class,
         ],
 
         // 'users' => [
